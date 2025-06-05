f4c2266fa4ea34a1d563df143b4954d0d545d7c4
Sujith-Kumar-2003 <sujithkumar2003k@gmail.com>
2025-06-05 15:10:36 -0400

    initial javascript code

--- JavaScript/first_javascript_file.js
+++ JavaScript/first_javascript_file.js
@@ -1,1 +1,12 @@
-alert("Hello world");
+function test(){
+    var a = 3;
+    var b = 8;
+
+    var c = a;
+    a = b;
+    b = c;
+
+    console.log("a value is: " + a);
+    console.log("b value is: " + b);
+}
+test()
