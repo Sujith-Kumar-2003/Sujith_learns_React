574c67657dcf351a940fa38b3bb8b5ac066b1848
Sujith-Kumar-2003 <sujithkumar2003k@gmail.com>
2025-06-11 16:29:06 -0400

    Added more intermediate js code

--- JavaScript/Intermediate/combining_comparators.js
+++ JavaScript/Intermediate/combining_comparators.js
@@ -2,7 +2,7 @@ var n = Math.random();
 n = Math.ceil(n * 100);
 console.log(n);
 if (n > 0 && n < 31){
-console.log("Least Compatible");
+console.log("Ditch that bitch");
 }
 if (n >= 31 & n < 71){
     console.log("Good compatibility");
